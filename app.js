c009e7300021b6b0e8ea547d4fe879fceacce3b5
Carlos Kruger <carloskr@yahoo.com>
2019-09-24 19:51:03 -0400

    getAllPosts function implemented

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+console.log('connect to app.js')
+
 function signUpUser(email,password,username){
     opts = { email: email, password: password, username: username}
-    fetch('http://thesi.generalassemb.ly:8080/signup', {
+    fetch(`http://thesi.generalassemb.ly:8080/signup`, {
         method: 'post',
         body: JSON.stringify(opts)
       }).then(function(response) {
@@ -14,12 +16,21 @@ function signUpUser(email,password,username){
 // signUpUser("carlos@Ga.com", "carlos", "carlos")
 
 function listAllPosts(){
+  let postsBoard = document.getElementById("all-post")
     fetch(`http://thesi.generalassemb.ly:8080/post/list`)
     .then((response) => {
         return response.json();
       })
       .then ((response) => {
-        console.log(response);
+          for (let i = 0; i < 30; i++){
+        let theTitle = document.createElement('h2')
+        theTitle.innerText = response[i].title
+        let theParagraph = document.createElement('p')
+        theParagraph.innerText = response[i].description
+        postsBoard.appendChild(theTitle)
+        postsBoard.appendChild(theParagraph)
+          }
+        console.log(response); 
       })
       .catch ((err) => {
         console.log(err);
@@ -28,3 +39,5 @@ function listAllPosts(){
         console.log('done');
       })
 }
+
+listAllPosts()
