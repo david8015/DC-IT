d65e32e3f25b8de573e3b19f65f235ed72bd9788
David Hernandez <david.hernandez8015@gmail.com>
2019-09-27 13:58:14 -0400

    display comments by post ID

--- app.js
+++ app.js
@@ -154,17 +154,135 @@ function login(mail, pwd){
         return response.json();
       })
       .then ((response) => {
-      console.log(response)
-       
+        // console.log('done');
+        // console.log(response.length);
+        if(response.length > 0){
+
+        let mainContent = document.querySelector("#main-content");
+        mainContent.className = "d-none";
+
+          // <div class="col-lg-8">
+           let mainDiv = document.createElement("div");
+           mainDiv.className = "col-lg-8";
+
+           document.querySelector("#main").appendChild(mainDiv);
+
+          //  console.log("test");
+          //  console.log(response[0].post.title);
+
+          {/* <!-- Title --> */}
+          // <h1 class="mt-4">Post Title</h1>
+          let title = document.createElement("H1");
+          title.className = "mt-4";
+          title.innerHTML = response[0].post.title;
+
+          // console.log("test 2" + response[0].post.title);
+          // console.log("test");
+
+         
+
+          mainDiv.appendChild(title);
+          {/* <!-- Author --> */}
+          // <p class="lead"></p>
+         let p = document.createElement("p");
+         p.className = "lead";
+         p.innerHTML = `create by ${response[0].post.user.username}`;
+
+         mainDiv.appendChild(p);
+
+          {/* <hr> */}
+         let hr = document.createElement("hr");
+          mainDiv.appendChild(hr);
+
+             {/* Post Content  */}
+          // <p class="lead">Lorem ipsum</p>
+          let postContent = document.createElement("p");
+          postContent.className = "lead";
+          postContent.innerHTML = response[0].post.description;
+
+          mainDiv.appendChild(postContent);
+
+          {/* <!-- Comments Form --> */}
+
+          for(let i=0; i<response.length; i++){}
+
+          //  <div class="card my-4">
+             let d1 = document.createElement("div");
+             d1.className = "card my-4";
+          
+             mainDiv.appendChild(d1);
+          //   <h5 class="card-header">Leave a Comment:</h5>
+               let h1 =  document.createElement("h5");
+               h1.className = "card-header";
+               h1.innerHTML = "Leave a Comment:";
+
+               d1.appendChild(h1);
+          //   <div class="card-body">
+                let d2 = document.createElement("div");
+                d2.className = "card-body";
+
+                d1.appendChild(d2);
+          //     <form>
+          let f = document.createElement("form");
+
+          d2.appendChild(f);
+          //       <div class="form-group">
+             let d3 = document.createElement("div");
+             d3.className = "form-group";
+
+             f.appendChild(d3);
+          //         <textarea class="form-control" rows="3"></textarea>
+           let t = document.createElement("textarea");
+            t.className= "form-control";
+            t.setAttribute = ("rows", 3);
+
+            d3.appendChild(t);
+          //       </div>
+          //       <button type="submit" class="btn btn-primary">Submit</button>
+          let d = document.createElement("button");
+          d.className = "btn btn-primary";
+          d.setAttribute = ("type", "submit");
+          d.innerHTML = "Submit";
+
+          f.appendChild(d);
+          //     </form>
+          //   </div>
+          // </div>
+
+          //  <!-- Single Comment --> 
+          //  <div class="media mb-4">
+          let d4 = document.createElement("div");
+          d4.className = "media mb-4"
+          
+         mainDiv.appendChild(d4);
+      
+          //   <div class="media-body">
+          let d5 = document.createElement("div");
+          d5.className = "media-body"
+
+          d4.appendChild(d5);
+          //     <h5 class="mt-0">Commenter Name</h5>
+          let commenterName = document.createElement("h5");
+          commenterName.className = "mt-0"
+          commenterName.innerHTML = response[0].user.username;
+
+          // console.log(response[0]);
+          // console.log(response[0].user);
+
+          d5.appendChild(commenterName);
+
+          let p2 = document.createElement("p");
+          p2.innerHTML = response[0].text;
+          d5.appendChild(p2);
+        }
       })
       .catch ((err) => {
         console.log(err);
       })
-      .finally (() => {
-         console.log('done');
-      
-      })
-}
+      .finally ((response) => {
+
+       })
+      }
 
   
 
@@ -242,7 +360,7 @@ function postEventListener(){
      console.log("event listener");
      console.log(event.target.id);
      getCommentsByPostId(event.target.id)
-     return event.target.id;
+    //  return event.target.id;
    });
  }
 };
